5fe4a57a12069cdc366318f8f9c4609b40e43324
fanyong557 <fanyong557@pingan.com.cn>
2015-08-26 15:13:02 +0800

    update test js

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,31 +1,3 @@
-/**
- * list view
- * 产品列表页
- **/
+(function() {
 
-define('js/view/list', ['js/util/api/baseApi'], function(Api) {
-
-    var view = Backbone.View.extend({
-
-        initialize: function(options, config) {
-            this.render();
-
-            console.log('test initialize');
-        },
-
-        render: function() {
-            var self = this;
-            self.$el.html(tpl('#view_list_tpl', {
-
-            }));
-
-            $('header.logo').show();
-            self._loadData();
-            
-            console.log('test render');
-
-        }        
-    });
-
-    return view;
-});
+})();
